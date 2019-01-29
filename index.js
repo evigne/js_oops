7c6c97f3349d666db8aa827030cc46b12243b33f
evigne <vigneshwaranethirajan@gmail.com>
2019-01-29 20:18:41 +0530

    adding useful links

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ let name = 'vigneshwaran';
 console.log(name);
 let lis = [1,2,3,4,5];
 console.log(lis.length);
+
+//  https://javascript.info/
+//  https://reactjs.org/docs/create-a-new-react-app.html
+//  https://medium.com/codingthesmartway-com-blog/pure-javascript-building-a-real-world-application-from-scratch-5213591cfcd6
+//  
+// Coding Addict
+// Traversy Media 
--- private_properties_and _methods_10.js
+++ private_properties_and _methods_10.js
@@ -1,5 +1,6 @@
+// before
 function Circle(radius){
-    let color = 'red' // not part of the object
+    let color = 'red' // not part of property of the object
     this.radius = radius;
     this.defaultLocation = {x:0,y:0};
     this.computeLocation = function(factor){
@@ -7,6 +8,23 @@ function Circle(radius){
     }
     this.draw = function(){
         this.computeLocation(0.1);
+        console.log("This is Constructor Function");
+    }
+}
+
+const another = new Circle(10);
+
+//after
+
+function Circle(radius){
+    let color = 'red' // not part of property of the object
+    this.radius = radius;
+    let defaultLocation = {x:0,y:0};
+    let computeLocation = function(factor){
+        //'''
+    }
+    this.draw = function(){
+        computeLocation(0.1);
         console.log("This is Constructor Function");
     }
 }
